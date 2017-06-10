9b64edf4cacfa3a50234a782baab85e315a87e47
Lei Xing <lei.xing@akqa.com>
2017-06-10 17:53:23 +0200

    debug handlebars one more time

--- templates/templates.js
+++ templates/templates.js
@@ -94,7 +94,7 @@ module.exports = {
 				'movements'
 			],
 			templates: [
-				"{{ title }} appartient {{#ifCond movements.length '>' 1}}aux courants artistiques {{else}} au courant artistique {{/ifCond}} {{#movements}}{{#ifCond @last }} {{else}}, {{/ifCond}}{{{ fields.name }}}{{/movements}}"
+				"{{ title }} appartient {{#ifCond movements.length '>' 1}}aux courants artistiques {{else}} au courant artistique {{/ifCond}} {{#movements}}{{#ifCond @last '==' true }} {{else}}, {{/ifCond}}{{{ fields.name }}}{{/movements}}"
 			]
 		}
 	],
