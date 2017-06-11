4665c7fdd0e4d129f8725b125eafcd0241bb2570
Lei Xing <lei.xing@akqa.com>
2017-06-11 18:55:45 +0200

    handle mainartists

--- Bernie.js
+++ Bernie.js
@@ -423,7 +423,7 @@ class Bernie {
 							//GET main artists of movements which this one is associated with
 							this.entity.movements
 							for (let i = 0; i < this.entity.movements.length; i++) {
-								artists.concat( this.entity.movements[i].mainArtists )
+								artists.concat( this.entity.movements[i].fields.mainArtists )
 							}
 						} else {
 							artists = this.entity[params]
