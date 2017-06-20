105e2648c5a42c4f4eae14e2c513b62769084c52
Eleve <eleve@P-212-iOS19.local>
2017-06-20 16:52:46 +0200

    add easter eggs debug

--- Bernie.js
+++ Bernie.js
@@ -15,8 +15,6 @@ const Utils = require('./helpers/utils.js');
 
 const EasterEgg = require('./database/easterEgg.js')
 
-console.log(EasterEgg);
-
 const WikiAPI = require('wikijs');
 const wiki = WikiAPI.default();
 
