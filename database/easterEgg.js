acef838ead05e1c03e8fea77ed434e2f4afc54c7
Eleve <eleve@P-212-iOS19.local>
2017-06-20 17:12:39 +0200

    add easter eggs debug

--- database/easterEgg.js
+++ database/easterEgg.js
@@ -13,7 +13,7 @@ module.exports = {
     'position': "Designer",
     'birthdate': "24/01/1992",
     'description': "Camcam est vraiment un sale petit con.",
-    'imageUrl': "https://scontent-cdt1-1.xx.fbcdn.net/v/t31.0-8/10658752_10152765862069334_7250078609464854556_o.jpg"
+    'imageUrl': "https://pbs.twimg.com/profile_images/378800000096121309/457608dc79657f01ba8ebba8341ffb9c_400x400.png"
   },
   {
     'firstName': "Thomas",
@@ -29,6 +29,6 @@ module.exports = {
     'position': "Développeur",
     'birthdate': "01/10/1994",
     'description': "Lei est vraiment un sale petit con.",
-    'imageUrl': "https://scontent-cdt1-1.xx.fbcdn.net/v/t31.0-8/17240642_10208225215299919_4795965410917710975_o.jpg"
+    'imageUrl': "https://pbs.twimg.com/profile_images/742352821081739264/FEP9n7Wu_400x400.jpg"
   }]
 }
